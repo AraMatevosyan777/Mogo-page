8d09bc9e10c1a7e3596df0e259b5b4dfb9376dc2
Aro Matevosyan <AroMatevosyan777@gmil.com>
2020-02-10 10:34:39 +0400

    added index.html file

---
+++ js/new.js
@@ -0,0 +1,54 @@
+$(function(){
+    var header = $("#header"),
+        introH = $("#intro").innerHeight(),
+        scrollOffset = $(window).scrollTop();
+
+
+
+// fixed header 
+    checkScroll(scrollOffset);
+    $(window).on("scroll",function(){
+        scrollOffset = $(this).scrollTop();
+        checkScroll(scrollOffset);
+    });
+    function checkScroll(scrollOffset){
+        if(scrollOffset>=introH){
+            header.addClass("fixed")
+        }
+        else{
+            header.removeClass("fixed")
+        }
+    }
+
+
+    $("[data-scroll]").on("click", function(event){
+        event.preventDefault();
+        var $this = $(this);
+        var blockId = $this.data("scroll"),
+            blockOffset = $(blockId).offset().top;
+
+
+            $("#nav a").removeClass("active");
+            $this.addClass("active");
+            $("html,body").animate({
+                scrollTop:blockOffset
+            },1000);
+    })
+
+    // menu nav toggle 
+
+    $("#nav_toggle").on("click",function(event){
+        event.preventDefault();
+        $(this).toggleClass("active");
+        $("#nav").toggleClass("active");
+    })
+
+
+    // collapse
+    $("[data-collapse]").on("click", function(event){
+        event.preventDefault();
+        var $this = $(this);
+        var blockId = $this.data("collapse");
+        $this.toggleClass("active");
+    })
+});
